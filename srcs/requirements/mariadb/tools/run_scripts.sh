72166eb22e8720ce86516dcc8e0a1108830680c6
Abdellah Aitouali <aaitoual@e2r4p6.1337.ma>
2023-03-14 00:38:01 +0100

    all the services are working, need some configurations on wordpress

--- srcs/requirements/mariadb/tools/run_scripts.sh
+++ srcs/requirements/mariadb/tools/run_scripts.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
-# sed -i "s+= mysql+= root+" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s+127.0.0.1+0.0.0.0+g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s+user                    = mysql+user                    = root+g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s+#port                   = 3306+port                    = 3306+g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-bash /scripts/config_mysql.sh
+sleep 2
 
-# service mysql stop && mysqld
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
+
+mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+mysqld_safe
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,4 +8,8 @@ sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/html/wordpress/wp-config.
 
 sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
 
-sed -i "s+/run/php/php7.3-fpm.sock+9099+" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/localhost/mariadb/g" /var/www/html/wordpress/wp-config.php
+
+sed -i "s+/run/php/php7.3-fpm.sock+0.0.0.0:9000+g" /etc/php/7.3/fpm/pool.d/www.conf
+
+php-fpm7.3 -F
