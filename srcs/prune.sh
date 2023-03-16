9f3521bdd9062f47225938594f3b6733d9377571
Abdellah Aitouali <aaitoual@e2r4p1.1337.ma>
2023-03-16 16:40:03 +0100

    the mandatory part is almost done, need some checks

--- srcs/prune.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# docker-compose down
-# docker volume rm srcs_mariadb_vol srcs_wordpress_vol
-# # docker system prune -fa 
-# docker-compose build
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,15 +2,6 @@
 
 sed -i "s+/run/php/php7.3-fpm.sock+0.0.0.0:9000+g" /etc/php/7.3/fpm/pool.d/www.conf
 
-# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-
-# sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" /var/www/html/wordpress/wp-config.php
-
-# sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/html/wordpress/wp-config.php
-
-# sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
-
-# sed -i "s/localhost/mariadb/g" /var/www/html/wordpress/wp-config.php
 wp core download --path=/var/www/html/wordpress --allow-root
 
 while ! wp config create --dbname=$WORDPRESS_DB_NAME \
@@ -22,9 +13,7 @@ while ! wp config create --dbname=$WORDPRESS_DB_NAME \
     sleep 3
 done
 
-wp core install --url=http://127.0.0.1:3000 --title=inception --admin_user=admin --admin_password=admin --admin_email=hhkirito07@gmail.com --path=/var/www/html/wordpress --allow-root
+wp core install --url=127.0.0.1 --title=inception --admin_user=admin --admin_password=admin --admin_email=hhkirito07@gmail.com --path=/var/www/html/wordpress --allow-root
 
 echo DONE!!!!!!!!!!!
-# wp option update siteurl $SITE_URL --allow-root --path="/var/www/html/wordpress"
-# wp option update home $SITE_URL --allow-root --path="/var/www/html/wordpress"
 php-fpm7.3 -F
